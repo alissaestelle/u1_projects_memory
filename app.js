89a075a5870a8c1c55629a783e10b1fb6a2ec5ec
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-03-07 16:37:08 -0500

    Push Test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const cOne = document.querySelector('#one')
 const cTwo = document.querySelector('#two')
+// const cThree = document.querySelector('#three')
 
 const turnCard = (button, image) => {
   button.style.backgroundImage = image
@@ -10,11 +11,22 @@ const turnCard = (button, image) => {
 
 cOne.addEventListener('click', () => {
   turnCard(cOne, "url('https://imgur.com/4I84VuB.jpg')")
+  // turnCard(cThree, "url('https://imgur.com/4I84VuB.jpg')")
 })
 
 cTwo.addEventListener('click', () => {
   turnCard(cTwo, "url('https://imgur.com/rAzmj18.jpg')")
 })
+
+// Brain Child Sanctuary:
+
+// Logic/Research:
+
+// Assigning the same image to two buttons at once, yet each button still waits for an individual click?
+// Assigning image URLs to global variables? (didn't work on the first try)
+// Randomize image URLs? How to control this, though? (so that each card has a match)
+
+// Functions:
 
 // const assignListeners = (arr) => {
 //   arr.forEach((elem) => {
@@ -24,5 +36,6 @@ cTwo.addEventListener('click', () => {
 //   })
 // }
 
-// Images
+// Images:
+
 // Card One: "url('https://imgur.com/4I84VuB.jpg')"
