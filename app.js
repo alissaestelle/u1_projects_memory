e844cd0be1c397640bb37c7118f9b814d7266aab
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-03-09 14:54:25 -0500

    2nd HTML Page Added

--- app.js
+++ app.js
@@ -1,22 +1,61 @@
+const nodeList = document.querySelectorAll('button')
+console.log(nodeList)
+const buttonArray = Array.from(nodeList)
+console.log(buttonArray)
+
+let sailorGuardians = [
+  "url('https://imgur.com/4I84VuB.jpg')",
+  "url('https://imgur.com/rAzmj18.jpg')",
+  "url('https://imgur.com/eKWUBnp.jpg)"
+]
+
+const usagiTsukino = "url('https://imgur.com/4I84VuB.jpg')"
+const reiHino = "url('https://imgur.com/rAzmj18.jpg')"
+const amiMizuno = "url('https://imgur.com/eKWUBnp.jpg)"
+
+// Class Notes:
+// const changeClass = (arr, idx) => {
+//   arr.forEach((arr, idx) => {
+//     if (idx === arr[0])
+//   }
+// }
+
+const oddCards = buttonArray.filter((button, idx) => {
+  return idx % 2 === 0
+})
+console.log(oddCards)
+
+// const oddImages = oddCards.map((card) => {
+//   card = sailorGuardians[0]
+//   sailorGuardians[0]++
+// })
+// console.log(oddImages)
+
+// for (let i = 0; i < sailorGuardians.length; i++) {
+//   while (oddCards.length < 10) oddCards[0] = sailorGuardians[i]
+//   oddCards[0]++
+// }
+
+const evenCards = buttonArray.filter((button, idx) => {
+  return idx % 2 !== 0
+})
+console.log(evenCards)
+
 const cOne = document.querySelector('#one')
-const cTwo = document.querySelector('#two')
-// const cThree = document.querySelector('#three')
+// const cTwo = document.querySelector('#two')
 
-const turnCard = (button, image) => {
-  button.style.backgroundImage = image
-  button.style.backgroundSize = '175px 175px'
-  button.style.backgroundRepeat = 'no-repeat'
-  button.style.backgroundPosition = 'center'
-}
+// const turnCard = (button, image) => {
+//   button.style.backgroundImage = image
+//   button.style.backgroundSize = '100px 100px'
+//   button.style.backgroundRepeat = 'no-repeat'
+//   button.style.backgroundPosition = 'center'
+// }
 
-cOne.addEventListener('click', () => {
-  turnCard(cOne, "url('https://imgur.com/4I84VuB.jpg')")
-  // turnCard(cThree, "url('https://imgur.com/4I84VuB.jpg')")
-})
+cOne.addEventListener('click', turnCard)
 
-cTwo.addEventListener('click', () => {
-  turnCard(cTwo, "url('https://imgur.com/rAzmj18.jpg')")
-})
+// cTwo.addEventListener('click', () => {
+//   turnCard(cTwo, "url('https://imgur.com/rAzmj18.jpg')")
+// })
 
 // Brain Child Sanctuary:
 
